c5d4cd51adbc0c56e02ebad6bd9f16eb8ef727fb
Gokul VR <106941614+Gokul-VR@users.noreply.github.com>
2026-02-10 13:31:44 +0530

    Fresher_page

---
+++ src/api/ib-freshser-page/controllers/ib-freshser-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ib-freshser-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ib-freshser-page.ib-freshser-page');
---
+++ src/api/ib-freshser-page/routes/ib-freshser-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ib-freshser-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ib-freshser-page.ib-freshser-page');
