083820b32a5f555c7fcc29a68a52713c6189027b
Vaishnav K V <vaishnav.kv@orderstack.io>
2026-02-11 13:47:48 +0530

    course hero api

---
+++ src/api/ib-course-page/routes/ib-course-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ib-course-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ib-course-page.ib-course-page');
---
+++ src/api/ib-course-page/services/ib-course-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ib-course-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ib-course-page.ib-course-page');
