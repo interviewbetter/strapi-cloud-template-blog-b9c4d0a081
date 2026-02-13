68bebdb91c18c87b416b08423962760b7cf3ee40
Neeraja C. R <neeraja.cr@orderstack.io>
2026-02-13 13:18:30 +0530

    faang

---
+++ src/api/ib-faang-page/routes/ib-faang-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ib-faang-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ib-faang-page.ib-faang-page');
---
+++ src/api/ib-faang-page/services/ib-faang-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ib-faang-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ib-faang-page.ib-faang-page');
