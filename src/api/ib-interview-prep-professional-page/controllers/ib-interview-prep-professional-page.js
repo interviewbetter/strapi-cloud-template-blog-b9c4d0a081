7860cde974f115edc158acd0db061a7f5214b9fa
Gokul VR <106941614+Gokul-VR@users.noreply.github.com>
2026-02-16 11:05:14 +0530

    Interview prep professional

---
+++ src/api/ib-interview-prep-professional-page/controllers/ib-interview-prep-professional-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ib-interview-prep-professional-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ib-interview-prep-professional-page.ib-interview-prep-professional-page');
---
+++ src/api/ib-interview-prep-professional-page/services/ib-interview-prep-professional-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ib-interview-prep-professional-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ib-interview-prep-professional-page.ib-interview-prep-professional-page');
