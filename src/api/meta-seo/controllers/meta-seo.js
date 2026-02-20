fab54cedd1f1c54cfca0c4a791febf660dd6b01e
Vaishnav K V <vaishnav.kv@orderstack.io>
2026-02-20 15:09:11 +0530

    meta seo added

---
+++ src/api/meta-seo/controllers/meta-seo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * meta-seo controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::meta-seo.meta-seo');
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1038,6 +1038,34 @@ export interface ApiLandingLanding extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiMetaSeoMetaSeo extends Struct.CollectionTypeSchema {
+  collectionName: 'meta_seos';
+  info: {
+    displayName: 'Meta-seo';
+    pluralName: 'meta-seos';
+    singularName: 'meta-seo';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::meta-seo.meta-seo'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    seoDetails: Schema.Attribute.JSON;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -1566,6 +1594,7 @@ declare module '@strapi/strapi' {
       'api::ib-home-page.ib-home-page': ApiIbHomePageIbHomePage;
       'api::landing-page.landing-page': ApiLandingPageLandingPage;
       'api::landing.landing': ApiLandingLanding;
+      'api::meta-seo.meta-seo': ApiMetaSeoMetaSeo;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
