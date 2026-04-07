105c1743ef9a2e14d43cc56828c269b47a7ac3b4
TNGONE-Nandhana <nandhana.v@orderstack.io>
2026-04-07 17:51:57 +0530

    u

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -133,7 +133,7 @@ export interface SharedExtras extends Struct.ComponentSchema {
     displayName: 'Extras';
   };
   attributes: {
-    desc: Schema.Attribute.Blocks;
+    description: Schema.Attribute.Blocks;
     Heading: Schema.Attribute.String;
   };
 }
