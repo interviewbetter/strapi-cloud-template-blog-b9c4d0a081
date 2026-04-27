73bb4698c9d40467df50f2d19be3d535cd4c7b54
TNGONE-Nandhana <nandhana.v@orderstack.io>
2026-04-27 17:40:02 +0530

    foundersbarauthor

---
+++ src/api/foundersbar-author/routes/foundersbar-author.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * foundersbar-author router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::foundersbar-author.foundersbar-author');
---
+++ src/api/foundersbar-author/services/foundersbar-author.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * foundersbar-author service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::foundersbar-author.foundersbar-author');
