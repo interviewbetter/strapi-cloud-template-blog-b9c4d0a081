ee872fdfcfbdbee3fefdcd75926914fbdec22912
TNGONE-Nandhana <nandhana.v@orderstack.io>
2026-05-11 10:15:23 +0530

    for creators herosection metatitle and description

---
+++ src/api/fb-for-creator/controllers/fb-for-creator.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fb-for-creator controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::fb-for-creator.fb-for-creator');
