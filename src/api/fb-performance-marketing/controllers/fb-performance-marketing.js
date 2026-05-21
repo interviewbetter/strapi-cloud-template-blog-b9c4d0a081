51434d1adfe161f3f59657cfe268c07b4f754fe3
TNGONE-Anusree <anusree.vinod@orderstack.io>
2026-05-21 14:49:11 +0530

    performance marketing page data

---
+++ src/api/fb-performance-marketing/controllers/fb-performance-marketing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fb-performance-marketing controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::fb-performance-marketing.fb-performance-marketing');
---
+++ src/api/fb-performance-marketing/services/fb-performance-marketing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fb-performance-marketing service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::fb-performance-marketing.fb-performance-marketing');
