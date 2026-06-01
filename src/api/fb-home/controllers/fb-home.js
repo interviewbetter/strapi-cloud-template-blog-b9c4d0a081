62644bd30b57006521ca0c358adffcff5b70af61
TNGONE-Nandhana <nandhana.v@orderstack.io>
2026-06-01 11:33:59 +0530

    FB homepage

---
+++ src/api/fb-home/controllers/fb-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fb-home controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::fb-home.fb-home');
---
+++ src/api/fb-home/services/fb-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fb-home service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::fb-home.fb-home');
