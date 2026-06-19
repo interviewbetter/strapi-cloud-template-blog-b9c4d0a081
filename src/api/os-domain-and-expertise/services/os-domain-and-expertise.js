d525c5a815195f61f787629913b7588b6260108b
TNGONE-Nandhana <nandhana.v@orderstack.io>
2026-06-19 15:11:15 +0530

    domain and expertise page

---
+++ src/api/os-domain-and-expertise/services/os-domain-and-expertise.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * os-domain-and-expertise service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::os-domain-and-expertise.os-domain-and-expertise');
