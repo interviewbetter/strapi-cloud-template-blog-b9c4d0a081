1e08acf18a82d89d352c3297a855056ead0a0c78
TNGONE-Anusree <anusree.vinod@orderstack.io>
2026-06-29 11:06:41 +0530

    compliance subpage data

---
+++ src/api/os-soc-2/routes/os-soc-2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * os-soc-2 router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::os-soc-2.os-soc-2');
---
+++ src/api/os-soc-2/services/os-soc-2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * os-soc-2 service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::os-soc-2.os-soc-2');
