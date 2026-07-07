dba40e3027ecd4c6a4a78a521d206dc829779074
TNGONE-Anusree <anusree.vinod@orderstack.io>
2026-07-07 12:02:24 +0530

    startups via foundersbar

---
+++ src/api/os-startups-via-foundersbar/services/os-startups-via-foundersbar.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * os-startups-via-foundersbar service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::os-startups-via-foundersbar.os-startups-via-foundersbar');
