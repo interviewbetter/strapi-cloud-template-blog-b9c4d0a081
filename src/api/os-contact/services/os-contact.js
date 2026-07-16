70153fff5c8b6fb4e5e128bfa90179dc49211ec7
TNGONE-Nandhana <nandhana.v@orderstack.io>
2026-07-16 11:47:12 +0530

    os contact page

---
+++ src/api/os-contact/services/os-contact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * os-contact service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::os-contact.os-contact');
