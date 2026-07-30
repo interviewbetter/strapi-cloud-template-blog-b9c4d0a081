a0b3548aac692b6cc99bf0e78f2eaeff9acbbc45
TNGONE-Nandhana <nandhana.v@orderstack.io>
2026-07-30 14:17:00 +0530

    MVP home page added

---
+++ src/api/mvp-home/controllers/mvp-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mvp-home controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::mvp-home.mvp-home');
---
+++ src/api/mvp-home/services/mvp-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mvp-home service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::mvp-home.mvp-home');
